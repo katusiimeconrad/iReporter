f4c9fadf6972e8b273d0907a670df94579c92839
Katusiime Conrad <katusiimeconrad@gmail.com>
2021-09-09 11:14:13 +0300

    Testing Commits

--- src/org/pahappa/systems/views/IReporter.java
+++ src/org/pahappa/systems/views/IReporter.java
@@ -4,6 +4,10 @@ public class IReporter {
 	
 	public static void main(String[] args) {
 		System.out.println("iReporter test");
+
+        System.out.println("Sean'z Branch");
+
+        System.out.println();
 	}
 	
 }
