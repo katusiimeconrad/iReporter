f23f082aaa448d8624916f4249368be865fa5d3d
benon123 <benontumukunde71@gmail.com>
2021-09-09 15:23:41 +0300

    i have implemented the incident class

--- src/org/pahappa/systems/models/Incident.java
+++ src/org/pahappa/systems/models/Incident.java
@@ -1,5 +1,5 @@
 package org.pahappa.systems.models;
-
+import java.util.Date;
 /**
  * The class {@code Incident} represents an occurrence, condition, 
  * or situation arising in the course of work that resulted in or could 
@@ -10,5 +10,55 @@ package org.pahappa.systems.models;
  *
  */
 public class Incident {
-
+    private String id;
+    private String title;
+    private String type;
+    private Status status;
+    private Date  createdOn;
+    private String comment;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	@Override
+	public String toString() {
+		return "Incident [id=" + id + ", title=" + title + ", type=" + type + ", status=" + status + ", createdOn="
+				+ createdOn + ", comment=" + comment + ", getId()=" + getId() + ", getTitle()=" + getTitle()
+				+ ", getType()=" + getType() + ", getStatus()=" + getStatus() + ", getCreatedOn()=" + getCreatedOn()
+				+ ", getComment()=" + getComment() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+    
 }
---
+++ src/org/pahappa/systems/models/Status.java
@@ -0,0 +1,6 @@
+package org.pahappa.systems.models;
+
+public enum Status {
+	under_investigation, rejected,resolved;
+
+}
