51f8bfb221224f436334b7762312122881c029f1
ttcollins <ttamalecollins@gmail.com>
2021-09-10 14:24:12 +0300

    Getting changes from develop

--- src/org/pahappa/systems/views/IReporter.java
+++ src/org/pahappa/systems/views/IReporter.java
@@ -1,9 +1,9 @@
 package org.pahappa.systems.views;
 
 public class IReporter {
-	
-	public static void main(String[] args) {
-		System.out.println("iReporter");
-	}
-	
+
+    public static void main(String[] args) {
+        System.out.println("iReporter");
+    }
+
 }
