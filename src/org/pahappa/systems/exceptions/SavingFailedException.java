c4c8559b3bacbe385b64a32cca50310c76ed901b
MutebiAlexKato <39887695+MutebiAlexKato@users.noreply.github.com>
2021-09-10 15:56:57 +0300

    "IR-11 Implementing the exceptions provided." (#6)
    * "Implementing the exceptions provided."
    
    Co-authored-by: [Mutebi Alex] <[mutebilxkato@gmail.com]>

--- src/org/pahappa/systems/exceptions/SavingFailedException.java
+++ src/org/pahappa/systems/exceptions/SavingFailedException.java
@@ -1,9 +1,10 @@
 package org.pahappa.systems.exceptions;
 public class SavingFailedException extends Exception {
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
+	public SavingFailedException(String message){
+		super("Reason for not saving:" + message);
+	}
 
 }
