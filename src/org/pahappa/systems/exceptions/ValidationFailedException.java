6bd5cada9d65d25704b1064964bfa305503b5094
[Mutebi Alex] <[mutebilxkato@gmail.com]>
2021-09-10 14:28:55 +0300

    "second commit."

--- src/org/pahappa/systems/exceptions/ValidationFailedException.java
+++ src/org/pahappa/systems/exceptions/ValidationFailedException.java
@@ -1,8 +1,9 @@
 package org.pahappa.systems.exceptions;
 public class ValidationFailedException extends Exception {
 
-	ValidationFailedException(String message){
-		super("Validation failure because" + message);
-	}
+
 	private static final long serialVersionUID = 1L;
+	ValidationFailedException(String s){
+		super(s);
+	}
 }
