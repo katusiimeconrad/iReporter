92affa141fa76e2b9c553cea54bff234a099bb1a
[Mutebi Alex] <[mutebilxkato@gmail.com]>
2021-09-10 15:24:15 +0300

    " had implemented an exception in this branch. my bad"

--- src/org/pahappa/systems/exceptions/SavingFailedException.java
+++ src/org/pahappa/systems/exceptions/SavingFailedException.java
@@ -6,7 +6,5 @@ public class SavingFailedException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public SavingFailedException(String s) {
-		super("Reason for not saving:" + s);
-	}
+
 }
