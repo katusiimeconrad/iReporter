4685871cc36d9a831aa6c3a5fe113e6d8c710fb6
[Mutebi Alex] <[mutebilxkato@gmail.com]>
2021-09-10 14:14:40 +0300

    "Implementing the exceptions provided."

--- src/org/pahappa/systems/exceptions/SavingFailedException.java
+++ src/org/pahappa/systems/exceptions/SavingFailedException.java
@@ -1,9 +1,9 @@
 package org.pahappa.systems.exceptions;
 public class SavingFailedException extends Exception {
 
-	/**
-	 * 
-	 */
+	public SavingFailedException(String message){
+		super("Incident not saved because" + message);
+	}
 	private static final long serialVersionUID = 1L;
 
 }
--- src/org/pahappa/systems/exceptions/ValidationFailedException.java
+++ src/org/pahappa/systems/exceptions/ValidationFailedException.java
@@ -1,8 +1,8 @@
 package org.pahappa.systems.exceptions;
 public class ValidationFailedException extends Exception {
 
-	/**
-	 * 
-	 */
+	ValidationFailedException(String message){
+		super("Validation failure because" + message);
+	}
 	private static final long serialVersionUID = 1L;
 }
