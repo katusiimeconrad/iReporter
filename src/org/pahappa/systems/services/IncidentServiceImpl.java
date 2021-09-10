ec49acf71a586683a2f7c06402d7d2b3ef078deb
ttcollins <ttamalecollins@gmail.com>
2021-09-10 21:19:56 +0300

    IR-3-Implementing-updating-an-incident

--- src/org/pahappa/systems/services/IncidentServiceImpl.java
+++ src/org/pahappa/systems/services/IncidentServiceImpl.java
@@ -1,19 +1,74 @@
 package org.pahappa.systems.services;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.pahappa.systems.enums.Status;
+import org.pahappa.systems.exceptions.SavingFailedException;
+import org.pahappa.systems.exceptions.ValidationFailedException;
 import org.pahappa.systems.models.Incident;
 
 public class IncidentServiceImpl implements IncidentService {
 
+	private  static ArrayList<Incident> incidents = new ArrayList<Incident>();
+	private static int incidentIds=0;
+
 	@Override
 	public Incident saveIncident(Incident incident) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+
+		if (incident.getTitle()==null){
+			throw new ValidationFailedException("Please enter title");
+		}
+		if (incident.getTitle().isEmpty()){
+			throw new ValidationFailedException("Please enter the title");
+		}
+		if (incident.getComment()==null){
+			throw new ValidationFailedException("Please enter comment");
+		}
+		if (incident.getComment().isEmpty()){
+			throw new ValidationFailedException("please enter comment.");
+		}
+
+
+
+
+		incident.setId(++incidentIds);
+		incident.setStatus(Status.DRAFT);
+		incident.setCreatedOn( new Date());
+		incidents.add(incident);
+
+		return incident;
+
 	}
 
 	@Override
 	public Incident updateIncident(Incident incident) throws Exception {
-		// TODO Auto-generated method stub
+		for(Incident item: incidents){
+
+//			Check if the parameter Title matches the records in the array
+			if(incident.getTitle() == item.getTitle()){
+
+//				Check if both the comment and title sections have been filled
+				if(incident.getTitle().isEmpty()){
+					throw new ValidationFailedException("Please enter title");
+				}else if(incident.getTitle()==null){
+					throw new ValidationFailedException("Please enter title");
+				}else if(incident.getComment().isEmpty()){
+					throw new ValidationFailedException("Please enter comment");
+				}else if(incident.getComment()==null){
+					throw new ValidationFailedException("Please enter comment");
+				}
+
+//				Update the sections if found not to be empty or null
+				item.setTitle(incident.getTitle());
+				item.setComment(incident.getComment());
+
+				return item;
+			}else{
+				throw new ValidationFailedException("This record does not exist.");
+			}
+		}
 		return null;
 	}
 
--- src/org/pahappa/systems/views/IReporter.java
+++ src/org/pahappa/systems/views/IReporter.java
@@ -1,9 +1,28 @@
 package org.pahappa.systems.views;
 
+import org.pahappa.systems.models.Incident;
+import org.pahappa.systems.services.IncidentServiceImpl;
+
 public class IReporter {
+	public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
-        System.out.println("iReporter");
-    }
+        System.out.println("iReporter test");
 
+		/*Use the code below to test updating an incident. Uncomment everything, run the code and you should see the
+		* new comment. Comment out the title or comment when you are trying to update and you will get an exception.
+		* Also uncomment the return comment in the incident model.*/
+//		IncidentServiceImpl helper = new IncidentServiceImpl();
+//		Incident user = new Incident();
+//		user.setTitle("The trial");
+//		user.setComment("Best book ever");
+//		System.out.println("=======Printing title to new record=========");
+//		System.out.println(helper.saveIncident(user));
+//		System.out.println("======Printing search result after update=========");
+//		Incident user1 = new Incident();
+//		user1.setTitle("The trial");
+//		user1.setComment("Another one");
+//		System.out.println(helper.updateIncident(user1));
+
+	}
+	
 }
