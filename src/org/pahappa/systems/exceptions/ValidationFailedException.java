df678ae7d24041921f071d745daaaa6b9dcbee5c
[Mutebi Alex] <[mutebilxkato@gmail.com]>
2021-09-10 16:43:46 +0300

    "changed to boolean operator and also catered for empty strings. "

--- src/org/pahappa/systems/exceptions/ValidationFailedException.java
+++ src/org/pahappa/systems/exceptions/ValidationFailedException.java
@@ -3,7 +3,7 @@ public class ValidationFailedException extends Exception {
 
 
 	private static final long serialVersionUID = 1L;
-	ValidationFailedException(String message){
+	public ValidationFailedException(String message){
 		super(message);
 	}
 }
